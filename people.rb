89c734e5f600f9af90cb92b244cf22243e800d43
Luke Walker <luke.t.walker@gmail.com>
2014-01-16 14:31:49 -0500

    created instance of new teacher and successfully called greeting method on it.

--- people.rb
+++ people.rb
@@ -1,15 +1,17 @@
 class Person
-attr_accessor :name
+  attr_accessor :name
 
-def initialize
-  @name = name
+  def initialize
+    @name = name
+  end
+
+  def greeting
+    puts "hi my name is #{name}"
+  end
+
 end
 
-end
-
-
-
-class Student
+class Student < Person
 
   def learn
     "I get it!"
@@ -17,10 +19,15 @@ class Student
 
 end
 
-class Teacher
+class Teacher < Person
 
   def teach
     "Everything in ruby is an object"
   end
 
 end
+
+instructor = Teacher.new
+instructor.name = "Chris"
+instructor.greeting
+
